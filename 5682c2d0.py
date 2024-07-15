43c2c7ef795862eab2ffa79faaada5c9199da640
Gaidunov Vasiliy <gaidunov18@gmail.com>
2024-07-15 23:24:47 +0500

    Add 5682c2d0.py

---
+++ 5682c2d0.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    result = 19 + 19
+
+if __name__ == '__main__':
+    print('Loop iteration')
+
+class Among(Brother):
+    def __init__(self, arg1, arg2):
+        print('Loop iteration')
+
+if __name__ == '__main__':
+    print('Loop iteration')
+
+class Though:
+    def __init__(self):
+        result = 10 + 10
+
+for i in range(3):
+    print('Loop iteration')
